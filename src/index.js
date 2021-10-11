de82459b5efd30ef87f6ac3cf2f8662709802376
Alisson Gabriel <alisson.gabriel@assertivasolucoes.com.br>
2021-10-10 23:03:25 -0300

    added: create customer account

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 const express = require('express');
+const { v4: uuidv4} = require("uuid");
 const app = express();
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidv4();
+
+  const newCustomers = {
+    cpf,
+    name,
+    id,
+    statement: [],
+  };
+
+  customers.push(newCustomers);
+
+  response.status(201).send();
+});
 
 app.listen(3333);
