80e1874b19ee3d12659a21a4975033413c8c4c24
Alisson Gabriel <alisson.gabriel@assertivasolucoes.com.br>
2021-10-13 22:22:07 -0300

    fixed: adjust customer statment typo

--- src/index.js
+++ src/index.js
@@ -5,19 +5,30 @@ app.use(express.json());
 
 const customers = [];
 
-function verifyExistsAccountWithCPF(request, response, next) {
+function shouldNotAlreadyExistsAccountWithCpf(request, response, next) {
+  const { cpf } = request.body;
+
+  if (customers.some((customer) => customer.cpf === cpf)) {
+    return response.status(400).json({ error: "Customer already exists"});
+  }
+
+  return next();
+}
+function shouldExistsAccountWithCPF(request, response, next) {
   const { cpf } = request.params;
+  console.log(cpf);
   const customerFind = customers.find((customer) => customer.cpf === cpf);
 
   if (!customerFind) {
     return response.status(400).json({ error: "Customer not found"});
   }
+  console.log(customerFind);
 
   request.customer = customerFind;
   return next();
 };
 
-app.post("/account", (request, response) => {
+app.post("/account", shouldNotAlreadyExistsAccountWithCpf, (request, response) => {
   const { cpf, name } = request.body;
   const id = uuidv4();
 
@@ -25,7 +36,7 @@ app.post("/account", (request, response) => {
     cpf,
     name,
     id,
-    statement: [],
+    statment: [],
   };
 
   customers.push(newCustomers);
@@ -33,9 +44,24 @@ app.post("/account", (request, response) => {
   response.status(201).send();
 });
 
-app.get("/statment/:cpf", verifyExistsAccountWithCPF, (request, response) => {  
+app.get("/statment/:cpf", shouldExistsAccountWithCPF, (request, response) => {  
   const { customer } = request;
   return response.json(customer)
-})
+});
+
+app.post("/deposit/:cpf", shouldExistsAccountWithCPF, (request, response) => {
+  const { customer } = request;
+  const { description, amount } = request.body;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  }
+
+  customer.statment.push(statementOperation);
+  return response.status(201).send()
+});
 
 app.listen(3333);
